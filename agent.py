b90b56f66aa42f8560bb2db6215eb3f9b94c9832
Joel Martin <joelmartin@equinix.com>
2025-04-25 00:39:41 +0000

    Initial working chat loop.

---
+++ agent.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from litellm import completion
+
+model = "github_copilot/gpt-4"
+extra_headers = {"editor-version": "vscode/1.85.1"}
+
+def main():
+    print("Simple Litellm Agent (Ctrl-C to quit)")
+    messages = [{"content": "You are an advanced coding agent", "role":"system"}]
+    while True:
+        try:
+            user_input = input("\u001b[94myou\u001b[0m> ")
+        except EOFError as e:
+            break
+        messages.append({"content": user_input, "role":"user"})
+        response = completion(
+            model=model,
+            extra_headers=extra_headers,
+            messages=messages)
+        resp_message = response.choices[0].message
+        print(f"\u001b[93m{model}\u001b[0m> {resp_message.content}")
+        messages.append(resp_message.model_dump())
+
+main()
