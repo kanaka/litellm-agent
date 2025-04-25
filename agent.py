721efa2c21e8c6c1ccf91a88f55ad2cb8e4ccadc
Joel Martin <joelmartin@equinix.com>
2025-04-25 00:40:06 +0000

    Add verbose debug in/out (with global "debug" flag).

--- agent.py
+++ agent.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 from litellm import completion
+from pprint import pprint
 
+debug = False
 model = "github_copilot/gpt-4"
 extra_headers = {"editor-version": "vscode/1.85.1"}
 
@@ -14,10 +16,16 @@ def main():
         except EOFError as e:
             break
         messages.append({"content": user_input, "role":"user"})
+        if debug:
+            print("Sending messages full JSON:")
+            pprint(messages)
         response = completion(
             model=model,
             extra_headers=extra_headers,
             messages=messages)
+        if debug:
+            print("AI response full JSON:")
+            pprint(response.model_dump())
         resp_message = response.choices[0].message
         print(f"\u001b[93m{model}\u001b[0m> {resp_message.content}")
         messages.append(resp_message.model_dump())
