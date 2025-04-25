a6388c903234db3578763a7b5a5b021891343996
Joel Martin <joelmartin@equinix.com>
2025-04-25 22:13:33 +0000

    Add ls_dir tool. Misc cleanup.

--- agent.py
+++ agent.py
@@ -4,16 +4,17 @@ import inspect
 import json
 from litellm import completion
 from pprint import pprint
+import subprocess
 
 debug = False
 model = "github_copilot/gpt-4"
 extra_headers = {"editor-version": "vscode/1.85.1"}
 
 
-def get_tools_param(tool_map):
+def get_tools_param(tools_map):
     typemap = {int: "integer", float: "number", bool: "boolean"}
     tools = []
-    for name, fn in tool_map.items():
+    for name, fn in tools_map.items():
         props, req = {}, []
         for p in inspect.signature(fn).parameters.values():
             if p.kind.name.startswith("VAR"):  # skip *args/**kw
@@ -36,16 +37,24 @@ def get_tools_param(tool_map):
     return tools
 
 def read_file(path):
-    return open(path).read()
+    """Read the contents of a file at path and return as a string"""
+    return json.dumps(open(path).read())
 
-TOOL_MAP = {
+def ls_dir(path):
+    """Run `ls -la path` and returns map with 'stdout', 'stderr', and 'returncode'"""
+    cp = subprocess.run(["ls", "-la", path], capture_output=True, text=True)
+    res = {k: getattr(cp, k) for k in ('stdout', 'stderr', 'returncode')}
+    return json.dumps(res)
+
+TOOLS_MAP = {
     "read_file": read_file,
+    "ls_dir": ls_dir,
 }
 
 def main():
     print("Simple Litellm Agent (Ctrl-C to quit)")
     messages = [{"content": "You are an advanced coding agent", "role":"system"}]
-    tools = get_tools_param(TOOL_MAP)
+    tools = get_tools_param(TOOLS_MAP)
 
     while True:
         if messages[-1]["role"] != "tool":
@@ -76,10 +85,10 @@ def main():
         if tool_calls:
             for tc in tool_calls:
                 fn = tc['function']
-                print(f"\u001b[92mtool_call:\u001b[0m> {fn.model_dump()}")
-                if fn.name == "read_file":
+                if fn.name in TOOLS_MAP:
                     fn_args = json.loads(fn.arguments)
-                    fn_result = read_file(**fn_args)
+                    print(f"\u001b[92mtool_call\u001b[0m> {fn.name}({fn_args})")
+                    fn_result = TOOLS_MAP[fn.name](**fn_args)
                     messages.append({
                         "role": "tool",
                         "tool_call_id": tc['id'],
